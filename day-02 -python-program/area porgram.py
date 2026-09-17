1458b81042626cf5dbb1e72cc24f4d228f23bf55
deepshigakumaresan-design <deepshigakumaresan@gmail.com>
2026-09-17 10:04:54 +0530

    Add files via upload

---
+++ day-02 -python-program/area porgram.py
@@ -0,0 +1,23 @@
+import math
+
+# Integer values
+radius = 5
+length = 10
+width = 4
+side = 6
+
+# Area calculations
+circle_area = math.pi * radius * radius
+rectangle_area = length * width
+square_area = side * side
+
+# Output
+print("Radius =", radius)
+print("Area of Circle =", circle_area)
+
+print("Length =", length)
+print("Width =", width)
+print("Area of Rectangle =", rectangle_area)
+
+print("Side =", side)
+print("Area of Square =", square_area)
